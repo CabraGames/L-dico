ccda785adafeb46c5a0f4d1dc847d78539b4b044
CabraGames <nascimento.machado.luiz@escola.pr.gov.br>
2025-10-30 09:29:15 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const nxt = document.querySelectorAll('.btn-next')
+const nxt = document.querySelectorAll('.btn-nxt')
 
 nxt.forEach(button => {
     button.addEventListener('click', function(){
         const atual = document.querySelector('.ativo')
-        const nxtStep = 'step-' + this.getAtributte('data-next')
+        const proximoPasso = 'step-' + this.getAtributte('data-nxt')
 
         atual.classList.remove('.ativo')
         document.getElementById(proximoPasso).classList.add('ativo')
